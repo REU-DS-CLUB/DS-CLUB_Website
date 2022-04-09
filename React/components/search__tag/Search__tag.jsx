53b81fafab77abad33f72c9d721e77489869c15c
REU-DS-CLUB <76436391+REU-DS-CLUB@users.noreply.github.com>
2022-04-09 20:16:25 +0300

    Add files via upload

---
+++ React/components/search__tag/Search__tag.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './search__tag.css';
+
+const Search__tag = () => {
+  return <div className='search_bg__tg'>
+      <input className="search_placeholder__tg" placeholder="Тег"/>
+  </div>;
+};
+
+export default Search__tag;
